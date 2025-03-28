736323ee0effda8c3722ab2c57267cf5d3c8eb1e
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-03-29 01:23:36 +0300

    shaml_admin

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'driver' => 'mysql',
         'host' => env('DB_HOST_USER', '127.0.0.1'),
         'port' => env('DB_PORT', '3306'),
-        'database' => env('DB_DATABASE_USER', 'laravel_user'),
+        'database' => env('DB_DATABASE_USER', 'shaml-user'),
         'username' => env('DB_USERNAME_USER', 'root'),
         'password' => env('DB_PASSWORD_USER', ''),
         'charset' => 'utf8mb4',
