6b047e5e0e05237293d83227292ad2cb541da68d
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-03-29 03:24:37 +0300

    shaml_admin

--- config/database.php
+++ config/database.php
@@ -42,19 +42,20 @@ return [
             'synchronous' => null,
         ],
 
-        'user_db' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST_USER', '127.0.0.1'),
-            'port'      => env('DB_PORT_USER', '3306'),
-            'database'  => env('DB_DATABASE_USER', 'laravel_user'),
-            'username'  => env('DB_USERNAME_USER', 'root'),
-            'password'  => env('DB_PASSWORD_USER', ''),
-            'charset'   => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix'    => '',
-            'strict'    => true,
-            'engine'    => null,
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
+
 
         'mysql' => [
             'driver' => 'mysql',
